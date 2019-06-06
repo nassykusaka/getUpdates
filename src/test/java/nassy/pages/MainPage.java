b206f894754941fc1be5509356de0547292db101
Anastasiia Chernyshova <anastasiia_chernyshova@epam.com>
2019-06-05 22:48:55 -0400

    Update structure

---
+++ src/test/java/nassy/pages/MainPage.java
@@ -0,0 +1,9 @@
+package nassy.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage extends AbstractPage {
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/nassy/runners/MainRunner.java
@@ -0,0 +1,52 @@
+package nassy.runners;
+
+import nassy.pages.MainPage;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import selenium.core.ui.DriverSingleton;
+
+public class MainRunner {
+    private static final String URL = System.getProperty("url");
+    private final static Logger logger = Logger.getLogger(MainRunner.class);
+    private MainPage mainPage;
+
+    @BeforeClass
+    public void setUp(){
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        logger.info("Closing browser");
+        DriverSingleton.kill();
+    }
+
+    @AfterMethod
+    public void screenShot(ITestResult result) {
+        if(ITestResult.FAILURE==result.getStatus()){
+            try {
+                mainPage.takeScreenshot();
+                logger.info("Taking a screenshot");
+            }catch (Exception e){
+                logger.error("Failed to take a screenshot");
+            }
+
+        }
+    }
+
+    @Test
+    public void checkNearestDate(){
+
+    }
+
+    @DataProvider
+    public Object[][] testData(){
+        return new Object[][]{
+                {"Some data#1"},
+                {"Some data#2"}
+        };
+    }
+
+}
