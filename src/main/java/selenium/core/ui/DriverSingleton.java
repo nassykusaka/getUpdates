064ed263a2960606542505a874b73da7c8aeba7d
Anastasiia Chernyshova <anastasiia_chernyshova@epam.com>
2019-06-09 18:38:28 -0400

    Update singleton

--- src/main/java/selenium/core/ui/DriverSingleton.java
+++ src/main/java/selenium/core/ui/DriverSingleton.java
@@ -4,8 +4,8 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.concurrent.TimeUnit;
+
 
 public class DriverSingleton {
 
@@ -25,7 +25,9 @@ public class DriverSingleton {
     private static WebDriver init(){
         ChromeOptions co = new ChromeOptions();
         co.addArguments("--start-maximized");
+        co.addArguments("--headless");
         driver = new ChromeDriver(co);
+        driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
