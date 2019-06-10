2710fd0ad28731a7d24f4dea27612d92f0144c18
Anastasiia Chernyshova <anastasiia_chernyshova@epam.com>
2019-06-09 20:24:05 -0400

    Add remote running option

--- src/main/java/selenium/core/ui/DriverSingleton.java
+++ src/main/java/selenium/core/ui/DriverSingleton.java
@@ -4,29 +4,50 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 
 public class DriverSingleton {
 
-    private DriverSingleton() { }
+    private DriverSingleton() {
+    }
 
     private static WebDriver driver;
     private final static Logger logger = Logger.getLogger(DriverSingleton.class);
+    private static String remoteURL = System.getenv("remoteURL");
 
-    public static WebDriver getWebDriverInstance() {
+    public static WebDriver getWebDriverInstance(String option) {
 
         if (driver != null) {
             return driver;
         }
-        return driver = init();
+        return driver = init(option);
     }
 
-    private static WebDriver init(){
+    private static WebDriver init(String option) {
         ChromeOptions co = new ChromeOptions();
-        co.addArguments("--start-maximized");
         co.addArguments("--headless");
-        driver = new ChromeDriver(co);
+        switch (option.toLowerCase()) {
+            case "local":
+                logger.info("Running driver locally");
+                driver = new ChromeDriver(co);
+                break;
+            case "remote":
+                try {
+                    logger.info("Running remote driver");
+                    driver = new RemoteWebDriver(new URL(remoteURL), DesiredCapabilities.chrome());
+                    break;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            default:
+                driver = new ChromeDriver(co);
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
